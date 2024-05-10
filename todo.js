e9d21d4ed0292a01da03e78df856da29461c80ed
codewithkaransingh <158612027+codewithkaransingh@users.noreply.github.com>
2024-05-10 15:24:38 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
 const port = 3000;
-const path = require("path");
-const fs = require("fs");
+
 const { title } = require("process");
 const cors = require("cors");
 const mongoose = require("mongoose");
